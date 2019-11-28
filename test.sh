755e0e44f62b0cb38c328cf803c5e47e3c6a6dea
yyleline <35785384+yyleline@users.noreply.github.com>
2019-11-28 00:54:04 -0500

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,6 @@
+#aws s3 cp s3://yyl-bucket/test RBItest --recursive
+
+
+#aws s3 cp /Users/yilingye/Desktop/wine.csv s3://yyl-bucket
+
+aws s3 cp s3://yyl-bucket/test/test1.jpg /Users/yilingye/Desktop
