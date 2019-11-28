e457e0e33f64cd2f6a5b6a7dd97ec0e1e5808ac3
yyleline <35785384+yyleline@users.noreply.github.com>
2019-11-28 01:08:59 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#aws s3 cp s3://yyl-bucket/test RBItest --recursive
-
-
-#aws s3 cp /Users/yilingye/Desktop/wine.csv s3://yyl-bucket
-
-aws s3 cp s3://yyl-bucket/test/test1.jpg /Users/yilingye/Desktop
